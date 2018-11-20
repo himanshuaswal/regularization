4890964591d503e32966a1e15733e1455dd1f0f8
himanshu aswal <haswal403@gmail.com>
2018-11-20 17:46:14 +0530

    load the data

--- regularization.py
+++ regularization.py
@@ -1,3 +1,6 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Lasso
+train_data = pd.read_csv('data.csv', header = None)
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
